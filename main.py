16aefeef5f269c2f00cfa81fc255f7dc6edad593
Kalu35 <luka.vuksanovic35@gmail.com>
2021-07-15 17:11:31 +0200

    Added Ship class, started with movement events

--- main.py
+++ main.py
@@ -29,6 +29,21 @@ YELLOW_LASER = pygame.image.load("assets/pixel_laser_yellow.png")
 BG = pygame.transform.scale(pygame.image.load("assets/background-black.png"), (WIDTH, HEIGHT))
 
 
+class Ship:
+
+    def __init__(self, x, y, health=100):
+        self.x = x
+        self.y = y
+        self.health = health
+        self.ship_img = None
+        self.laser_img = None
+        self.lasers = []
+        self.cool_down_counter = 0
+
+    def draw(self, window):
+        pygame.draw.rect(window, (255, 0, 0), (self.x, self.y, 50, 50))
+
+
 # ---- MAIN LOOP
 
 def main():
@@ -36,17 +51,20 @@ def main():
     FPS = 60
     level = 1
     lives = 5
+    ship = Ship(335, 650)
+
     main_font = pygame.font.SysFont("comicsans", size=50)
 
     clock = pygame.time.Clock()
 
     def refresh_redraw_window():
         WIN.blit(BG, (0, 0))
-        # ---- drawing text
-        lives_label = main_font.render(f'Lives: {lives}', 1, (255, 255, 255))
-        level_label = main_font.render(f'Level: {level}', 1, (255, 255, 255))
+        # ---- drawing stuff
+        lives_label = main_font.render(f'Lives: {lives}', True, (255, 255, 255))
+        level_label = main_font.render(f'Level: {level}', True, (255, 255, 255))
         WIN.blit(lives_label, (10, 10))
         WIN.blit(level_label, (WIDTH - lives_label.get_width() - 10, 10))
+        ship.draw(WIN)
         pygame.display.update()
 
     while run:
@@ -55,6 +73,10 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+
+        keys = pygame.key.get_pressed()  # dict of pressed keys
+        if keys[pygame.K_a]:  # moving left
+            ship.x -= 1
 
 
 main()
